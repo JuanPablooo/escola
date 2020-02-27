f1ebcd130eb4555f2854c0015a8fd50bab525849
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-02-26 23:06:18 -0300

    dando permicoes de requisicoes apenas para usuarios cadastrados e verificando tipo de permissao de usuario

---
+++ src/main/java/br/com/devdojo/PasswordEncoder.java
@@ -0,0 +1,10 @@
+package br.com.devdojo;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(bCryptPasswordEncoder.encode("123"));
+    }
+}
--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -10,6 +10,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +45,9 @@ public class StudentEndPoint {
 
     //@RequestMapping(method = RequestMethod.GET, path = "/{id}")
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id){
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id,
+                                            @AuthenticationPrincipal UserDetails userDetails){
+        System.out.println(userDetails);
         verifyIfStudentExists(id);
         Student student = studentDAO.findOne(id);
         return  new  ResponseEntity<>(student, HttpStatus.OK);
@@ -63,6 +68,7 @@ public class StudentEndPoint {
 
     //@RequestMapping(method = RequestMethod.DELETE)
     @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable("id") Long id){
         verifyIfStudentExists(id);
         studentDAO.delete(id);
